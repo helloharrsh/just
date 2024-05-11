8869fd3d416c26ccb2c1bc6de63de811c3224072
Harsh Tripathi <139077253+helloharrsh@users.noreply.github.com>
2024-05-11 19:27:29 +0000

    updated till navbar.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,62 @@
-import React from "react";
-import {RiMenu3line, RiCloseLin} from 'react-icons/ri';
-import './navbar.css'
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/logo.svg';
+import './navbar.css';
 
-const navbar= () => {
-    return (
-        <div className="gpt3__navbar">
-            <div className="gpt3__navbar-links">
-                <div className="gpt3__navbar-links_logo">
-                    <img src = {logo} alt= "logo" />
-                </div>
-        </div>
-        </div>
-    )
-}
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
 
-export default navbar;  
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt = "logo" />
+        </div>
+        <div className="gpt3__navbar-links_container">
+        <p><a href="#blog">Library</a></p> 
+         
+          {/*. menu section can be updated later. 
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">What is GPT3?</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p> 
+         
+           */}
+
+        </div>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <button type="button">GitHub!</button>
+      </div>
+      <div className="gpt3__navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
+        {toggleMenu && (
+        <div className="gpt3__navbar-menu_container scale-up-center">
+          <div className="gpt3__navbar-menu_container-links">
+          <p><a href="#blog">Library</a></p> 
+            
+            {/*. menu section can be updated later. 
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">What is GPT3?</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p> 
+         
+           */}
+
+
+          </div>
+          <div className="gpt3__navbar-menu_container-links-sign">
+            <button type="button">GitHub!</button>
+          </div>
+        </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
